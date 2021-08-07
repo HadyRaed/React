30135c87cca691ad2f57d59c1a6672ad01cb2c09
hadyraed <hraed666@gmail.com>
2021-08-07 17:25:29 +0200

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>
+        March 28th
+        <div>
+          <h2>Car Insurance</h2>
+        </div>
+        <div>$294.6</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
