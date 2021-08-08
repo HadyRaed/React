5969f431ae416927f4f922cea4b98cd4481804c3
hadyraed <hraed666@gmail.com>
2021-08-08 13:27:45 +0200

    expenses

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 function App() {
+  const expenses = [
+    { title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 28) },
+    { title: "House Insurance", amount: 123.31, date: new Date(2021, 1, 2) },
+    { title: "Medical Insurance", amount: 241, date: new Date(2020, 12, 11) },
+    { title: "Club Insurance", amount: 124.67, date: new Date(2021, 2, 15) },
+  ];
   return (
     <div>
       <h2>Hady is here!</h2>
-      <ExpenseItem></ExpenseItem>
+      <Expenses items={expenses}/>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,14 @@
-function ExpenseItem() {
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+function ExpenseItem(props) {
   return (
     <div>
-      <div>
-        March 28th
-        <div>
-          <h2>Car Insurance</h2>
+      <div className="expense-item">
+        <ExpenseDate date={props.date}/>
+        <div class="expense-item__description">
+          <h2>{props.title}</h2>
         </div>
-        <div>$294.6</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
